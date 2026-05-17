d3b75d8184af67df81cf4d7debb748fdab00c5c6
cosmin ciolacu <cosminciolacu99@gmail.com>
2026-05-17 19:32:46 +0300

    feat: add cache clearing after creating and editing custom options

--- app/Filament/Resources/CustomOptions/Pages/CreateCustomOption.php
+++ app/Filament/Resources/CustomOptions/Pages/CreateCustomOption.php
@@ -4,8 +4,14 @@ namespace App\Filament\Resources\CustomOptions\Pages;
 
 use App\Filament\Resources\CustomOptions\CustomOptionResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Cache;
 
 class CreateCustomOption extends CreateRecord
 {
     protected static string $resource = CustomOptionResource::class;
+
+    protected function afterCreate(): void
+    {
+        Cache::forget('active_custom_options');
+    }
 }
--- app/Filament/Resources/CustomOptions/Pages/EditCustomOption.php
+++ app/Filament/Resources/CustomOptions/Pages/EditCustomOption.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\CustomOptions\Pages;
 use App\Filament\Resources\CustomOptions\CustomOptionResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Cache;
 
 class EditCustomOption extends EditRecord
 {
@@ -15,5 +16,10 @@ class EditCustomOption extends EditRecord
         return [
             DeleteAction::make(),
         ];
+    }
+
+    protected function afterSave(): void
+    {
+        Cache::forget('active_custom_options');
     }
 }
